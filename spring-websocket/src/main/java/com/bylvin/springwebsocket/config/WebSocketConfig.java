edca3fcd91500124c4adef4996f3556d2da1182e
Bylvin <carel.raditya@gmail.com>
2026-01-02 20:12:46 +0700

    Define websocket configuration

---
+++ spring-websocket/src/main/java/com/bylvin/springwebsocket/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.bylvin.springwebsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		// Broadcasting messages endpoint
+		config.enableSimpleBroker("/topic");
+
+		// Client access endpoint
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws") // Define endpoint which client use to establish connection
+		.setAllowedOrigins("*") // Define the url that can access back end app
+		.withSockJS(); // Define fallback for browser
+	}
+}
